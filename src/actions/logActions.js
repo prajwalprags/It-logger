deaba1b3fe9fb4ba86cc9c963127c66d7b9c135d
Prajwal Ravi <prajwalprags5@gmail.com>
2021-06-11 23:15:49 +0530

    Connecting Redux To a Component

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -13,29 +13,25 @@ import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types';
 //  });
 //};
 // };
-
+// Get logs from server
 export const getLogs = () => async (dispatch) => {
-    try {
-        setLoading();
+  try {
+    setLoading();
 
-        const res = await fetch('/logs');
-        const data = await res.json();
-    
-        dispatch({
-          type: GET_LOGS,
-          payload: data,
-        });
-      };
-    };
-     
-    } catch (err) {
-        dispatch({
-            type: LOGS_ERROR,
-            payload: err.response.data
-        });
-        
- }
- 
+    const res = await fetch('/logs');
+    const data = await res.json();
+
+    dispatch({
+      type: GET_LOGS,
+      payload: data,
+    });
+  } catch (err) {
+    dispatch({
+      type: LOGS_ERROR,
+      payload: err.response.data,
+    });
+  }
+};
 
 // Set loading to true
 export const setLoading = () => {
